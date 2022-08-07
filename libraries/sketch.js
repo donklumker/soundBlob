3e02c5a3ed836c2cbbc9683143b2820a007869d2
don klumker <doctorklum@gmail.com>
2022-08-07 14:20:15 -0700

    blob is now class

--- libraries/sketch.js
+++ libraries/sketch.js
@@ -2,89 +2,117 @@
 // next step classify the blob 
 
 let mic;
-let yoff = 0;
 let blob0;
+let blob1;
 let vol;
+// let xx = width/2;
+// let yy = height/2;
 // let start = 0;
 
 
 
 function setup() {
 
-  createCanvas(400, 400, );
+  createCanvas(300, 300);
 
   // initiated p5 Audio mic
   mic = new p5.AudioIn();
   mic.start();
   vol = mic.getLevel();
-  
 
 
-  blob0 = new Slob();
+
+  blob0 = new Slob(0, 0, 20, 0.002, 220);
+  blob1 = new Slob(100, 125, 75, 0.001, 60);
 
 }
 
 function draw() {
   background(220);
+  blob1.drawBlob();
   blob0.drawBlob();
+ 
 
 }
 
 class Slob {
-  constructor() {
+  constructor(xPos, yPos, dim, lerp, fl) {
+    
+    this.fl = fl;
+    this.dim = dim;
+    this.lerp = lerp;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.x = 0;
+    this.y = 0;
     this.start = 0;
     this.vol;
-   
-    
+    this.xoff = 0;
+    this.yoff = 0;
+    this.noiseMax;
+    this.phase = .01;
+
+
+
 
   }
 
   drawBlob() {
     //get volume value between 0 and 1.
     this.vol = mic.getLevel();
-  
-    this.smoothVol = lerp(this.start, this.vol, 0.00028);
-    //print (this.smoothVol);
- 
-    this.smoothMap = map(this.smoothVol, 0, 1, 0, 8000);
-    print (this.smoothMap); // map values to larger values
-    text (this.smoothMap, 0, 100,);
-    this.radius = this.smoothMap * 1000; //
- 
 
-    translate(width / 2, height / 2.62);
+    this.smoothVol = lerp(this.start, this.vol, this.lerp);
+    //print(this.smoothVol);
+
+    this.smoothMap = map(this.smoothVol, 0, 1, 0, 800);
+    //print(this.smoothMap); // map values to larger values
+
+    text(this.smoothMap, 150, 100);
+    text("smoothMap =", 40, 100);
+
+
+
+    this.radius = this.smoothMap * 100;
+    text(this.radius, 150, 110);
+    text("radius =", 40, 110);
+
+
+    //text(this.noiseMax, 20, 120);
+
+
+
+
+    translate(this.xPos, this.yPos);
+
+
+
+    // print(translate);
 
     noStroke();
-    fill(0);
+    fill(this.fl);
     beginShape();
-    this.noiseMax = this.smoothMap * 90;
-    push();
-    
-    translate(0, 100);
-    text('noiseMax =', -50, 100);
-    text(noiseMax, 25, 100);
+    this.noiseMax = this.smoothMap * 400;
+    //print (this.noiseMax);
 
-    text('radius =', -50, 110);
-    text(radius, 25, 110);
-
-    text('phase =', -50, 120);
-    text(phase, 25, 120);
-    pop();
+    //text('this.noiseMax', 40, 120);
 
 
-    for (let a = 0; a < TWO_PI; a += TWO_PI / 600) {
+
+    for (let a = 0; a < TWO_PI; a += TWO_PI / 300) {
       this.xoff = map(cos(a + this.phase), -1, 1, 0, this.noiseMax);
       this.yoff = map(sin(a + this.phase), -1, 1, 0, this.noiseMax);
-      let r = map(noise(this.xoff, this.yoff), 0, 1, 100, this.radius + 50);
-      let x = this.r * cos(a);
-      let y = this.r * sin(a);
+      this.r = map(noise(this.xoff, this.yoff), 0, 1, 100, this.radius + this.dim);
+      this.x = this.r * cos(a);
+      //print (this.x);
+      this.y = this.r * sin(a);
 
       vertex(this.x, this.y);
 
 
+
+
     }
     endShape(CLOSE);
-    this.zoff += this.smoothMap * 2;
-    this.phase += this.smoothMap * 2;
+    this.phase += .01;
   }
 }
